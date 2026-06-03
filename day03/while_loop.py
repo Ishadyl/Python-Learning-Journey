1ea3b46ba59c7d2539a1bf96e332eaa28a5eac63
Ishadyl <240160255005.isha@gdgu.org>
2026-06-04 04:34:01 +0530

    Day03 - Learning while loop

---
+++ day03/while_loop.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the number: "))
+
+while a>4:
+    a = int(input("Enter your next number: "))
+else:
+    print("loop ended")
+
+
+
+name = input("Enter the name: ")
+while name == "isha":
+    name1 = input("Enter the surname: ")
+    print(name  + name1)
+    name = "akriti"
+else:
+    name = input("Enter the name: ")
+
